aa67304793c4bcdae09433d7e053351bd64b6350
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2021-05-31 18:51:09 -0300

    Update Client.java

--- src/main/java/fast_reset/client/Client.java
+++ src/main/java/fast_reset/client/Client.java
@@ -5,9 +5,18 @@ import net.fabricmc.api.ModInitializer;
 public class Client implements ModInitializer {
     public static boolean saveOnQuit;
 
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "fastreset";
+    public static final String MOD_NAME = "Fast Reset";
+
     @Override
     public void onInitialize() {
         saveOnQuit = true;
-        System.out.println("Using Fast Reset");
+        log(Level.INFO, "Using Fast Reset");
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
     }
 }
