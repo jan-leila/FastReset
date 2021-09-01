1933495c7a3f9a1bb3ae012d859f110bbdc8697d
Leyla <jan-leila@protonmail.com>
2021-08-31 20:53:31 -0500

    fixed world lock issue

--- src/main/java/fast_reset/client/Client.java
+++ src/main/java/fast_reset/client/Client.java
@@ -4,10 +4,13 @@ import net.fabricmc.api.ModInitializer;
 
 public class Client implements ModInitializer {
     public static boolean saveOnQuit;
+    public static boolean saving;
+    public static final Object saveLock = new Object();
 
     @Override
     public void onInitialize() {
         saveOnQuit = true;
+        saving = false;
         System.out.println("Using Fast Reset");
     }
 }
--- src/main/java/fast_reset/client/mixin/ResetMixin.java
+++ src/main/java/fast_reset/client/mixin/ResetMixin.java
@@ -4,16 +4,20 @@ import fast_reset.client.Client;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.level.storage.LevelStorage;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.io.IOException;
 import java.util.Iterator;
-
 
 @Mixin(MinecraftServer.class)
 public class ResetMixin {
+    @Shadow @Final protected LevelStorage.Session session;
+
     // kill save on the shutdown
     @Redirect(method = "shutdown", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z", ordinal = 0))
     private boolean getWorldsInjectOne(Iterator<ServerWorld> iterator){
@@ -23,6 +27,34 @@ public class ResetMixin {
         while(iterator.hasNext()){
             iterator.next().savingDisabled = true;
         }
+        return false;
+    }
+
+    @Redirect(method = "shutdown", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z", ordinal = 1))
+    private boolean getWorldsInjectTwo(Iterator<ServerWorld> iterator){
+        if(Client.saveOnQuit){
+            return iterator.hasNext();
+        }
+
+        new Thread(() -> {
+            synchronized(Client.saveLock){
+                while(iterator.hasNext()){
+                    ServerWorld world = iterator.next();
+                    if (world != null) {
+                        try {
+                            world.close();
+                        } catch (IOException ignored) {
+
+                        }
+                    }
+                }
+                try {
+                    this.session.deleteSessionLock();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
         return false;
     }
 
